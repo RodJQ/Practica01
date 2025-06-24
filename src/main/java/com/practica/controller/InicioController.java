c2ca026f907f122b3c68dddd7e75d4ad0952dd83
Rodney <Rodney@DESKTOP-E2C16P2>
2025-06-23 20:00:55 -0600

    Actualización de parte 3.1

--- src/main/java/com/practica/controller/InicioController.java
+++ src/main/java/com/practica/controller/InicioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InicioController {
 
     @GetMapping("/")
-    public String inicio() {
-        return "index";
+    public String mostrarInicio() {
+        return "index"; 
     }
 }
